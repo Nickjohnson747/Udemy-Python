212f515393a1329969e29231369146e722afc382
Nick Johnson <Nickjohnson747@gmail.com>
2024-08-28 11:26:21 -0700

    Added clock, error handling

--- To-Do List/gui.py
+++ To-Do List/gui.py
@@ -1,6 +1,8 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="new_todo")
 add_button = sg.Button("Add")
@@ -14,6 +16,7 @@ exit_button = sg.Button("Exit")
 window = sg.Window(
     "To-Do App",
     layout=[
+        [clock],
         [label],
         [input_box, add_button],
         [list_box, edit_button, complete_button],
@@ -23,9 +26,10 @@ window = sg.Window(
 )
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(event)
+    # print(values)
 
     match event:
         case "Add":
@@ -35,23 +39,27 @@ while True:
             window["todos"].update(values=todo_list)
 
         case "Edit":
-            todo_list = functions.get_todos()
-            idx_to_edit = todo_list.index(values["todos"][0])
+            try:
+                todo_list = functions.get_todos()
+                idx_to_edit = todo_list.index(values["todos"][0])
 
-            # Have to strip new line from list_box and re-add so values don't write onto wrong lines
-            todo_list[idx_to_edit] = values["new_todo".strip("\n")] + "\n"
-            functions.edit_todos(todo_list)
+                # Have to strip new line from list_box and re-add so values don't write onto wrong lines
+                todo_list[idx_to_edit] = values["new_todo".strip("\n")] + "\n"
+                functions.edit_todos(todo_list)
 
-            window["todos"].update(values=todo_list)
-
+                window["todos"].update(values=todo_list)
+            except IndexError as e:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
         case "Complete":
-            todo_list = functions.get_todos()
-            completed_todo = values["todos"][0]
-            todo_list.remove(completed_todo)
-            functions.edit_todos(todo_list)
+            try:
+                todo_list = functions.get_todos()
+                completed_todo = values["todos"][0]
+                todo_list.remove(completed_todo)
+                functions.edit_todos(todo_list)
 
-            window["todos"].update(values=todo_list)
-
+                window["todos"].update(values=todo_list)
+            except IndexError as e:
+                sg.popup("Please select an item first", font=("Helvetica", 20))
         case "Exit":
             break
 
